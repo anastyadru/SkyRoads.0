d7542c197b91b9d35706c119468973978d37e14d
Nastia <adruts@mail.ru>
2024-03-06 14:34:02 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    убрала this в ScoreManager

--- Assets/Client/Scripts/ScoreManager.cs
+++ Assets/Client/Scripts/ScoreManager.cs
@@ -55,7 +55,7 @@ public class ScoreManager : MonoBehaviour
 
     public void ToggleBoostedSpeed(bool isBoosted)
     {
-        this.isBoosted = isBoosted;
+        isBoosted = isBoosted;
     }
 
     private void OnDisable()
