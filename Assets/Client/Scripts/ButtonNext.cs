b3ceab660c41339b4f15e1eb188a027735c98593
Nastia <adruts@mail.ru>
2024-05-15 16:46:35 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    Отредактировала код ButtonNext

--- Assets/Client/Scripts/ButtonNext.cs
+++ Assets/Client/Scripts/ButtonNext.cs
@@ -8,15 +8,7 @@ public class ButtonNext : MonoBehaviour
 {
     public bool NextGame;
     private GameObject nextGameMenu;
-    
-    void Update()
-    {
-        if (Input.GetKeyDown("Next"))
-        {
-            LoadMenu();
-        }
-    }
-    
+
     public void LoadMenu()
     {
         SceneManager.LoadScene("Menu");
