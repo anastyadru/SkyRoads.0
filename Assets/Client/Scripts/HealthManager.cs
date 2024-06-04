a47be12ec0c34913f9904fbd2dc5bafc8bca9b96
Nastia <adruts@mail.ru>
2024-06-04 17:13:33 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    Создала скрипт HealthManager

---
+++ Assets/Client/Scripts/HealthManager.cs
@@ -0,0 +1,49 @@
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private Text HealthText;
+    
+    public float health = 100f;
+
+    public void Start()
+    {
+        UpdateHealthText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Asteroid"))
+        {
+            health -= 20;
+            UpdateHealthText();
+        }
+    }
+    
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            EndGame();
+        }
+    }
+    
+    private void UpdateHealthText()
+    {
+        HealthText.text = "HEALTH: " + health.ToString();
+    }
+    
+    private void EndGame()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Spaceship");
+        player.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
--- Assets/Client/Scripts/Spaceship.cs
+++ Assets/Client/Scripts/Spaceship.cs
@@ -5,7 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class Spaceship : MonoBehaviour
 {
@@ -48,20 +47,5 @@ public class Spaceship : MonoBehaviour
         }
 
         transform.rotation = Quaternion.Euler(0, 0, rotationX);
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Asteroid"))
-        {
-            EndGame();
-        }
-    }
-
-    void EndGame()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        player.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
