54ec6bb14bd969c8f38a5e346612e7a5a72e357c
Nastia <adruts@mail.ru>
2024-07-22 18:59:49 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    Изменила скрипт ScrollTexture

--- Assets/Client/Scripts/ScrollTexture.cs
+++ Assets/Client/Scripts/ScrollTexture.cs
@@ -1,8 +1,5 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollTexture : MonoBehaviour
@@ -12,6 +9,7 @@ public class ScrollTexture : MonoBehaviour
     private void Update()
     {
         _offset -= 0.2f;
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, _offset);
+        var renderer = GetComponent<Renderer>();
+        renderer.material.mainTextureOffset = new Vector2(0, offset);
     }
 }
