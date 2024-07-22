2f8a055a4c1d7744dafab530e750f49620abbe8d
Nastia <adruts@mail.ru>
2024-07-22 18:42:24 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    Изменила скрипт ButtonNext

--- Assets/Client/Scripts/ButtonNext.cs
+++ Assets/Client/Scripts/ButtonNext.cs
@@ -1,15 +1,11 @@
 // Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.using System;
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonNext : MonoBehaviour
 {
     public bool NextGame;
-    private GameObject nextGameMenu;
 
     public void LoadMenu()
     {
