8358e046619fc150fbb3f7af2a39a2a0c1678c86
Nastia <adruts@mail.ru>
2024-09-18 21:04:48 +0300

    https://github.com/anastyadru/SkyRoads.0.git
    Отредактировала скрипт Baundary

--- Assets/Client/Scripts/Baundary.cs
+++ Assets/Client/Scripts/Baundary.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+// Copyright (c) 2012-2024 FuryLion Group. All Rights Reserved.
+
 using UnityEngine;
 
 public class Baundary : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(other.gameObject);
     }
 }
